0cab546b1fc18a409e726d50ac3f087919a9cdcc
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 03:31:11 +0200

    Refactor ExampleModule's Node component to pass properties to BaseModule
    The ExampleModule's Node component has been modified to accept properties, which are then passed to the BaseModule component. This allows for better customization and reusability of the module.

--- src/modules/ExampleModule/Node.tsx
+++ src/modules/ExampleModule/Node.tsx
@@ -1,9 +1,13 @@
 // Node that will be used to render the module in the flow view
 
+import { NodeProps } from "reactflow";
 import BaseModule from "../../components/Modules/BaseModule";
 
-export default function Node() {
+export default function Node(props : NodeProps) {
+
    return (
-      <BaseModule label="Example Module" />
+      <BaseModule 
+         {...props}
+      />
    )
 }
