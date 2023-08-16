2386ea6114926ec4195659a0dd2152c62b7541d0
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 04:33:40 +0200

    Refactor BaseModule component to use the `useInspector` hook

--- src/components/Modules/BaseModule.tsx
+++ src/components/Modules/BaseModule.tsx
@@ -1,34 +1,16 @@
-import {useEffect, useState} from 'react'
 import { Handle, NodeProps, Position } from 'reactflow'
-import { useInspectorStore } from '../../pages/Flow/InspectorStore'
+import { useInspector } from '../../pages/Flow/InspectorStore'
 import modules from '../../modules'
 
-export default function BaseModule({type, data} : NodeProps){
+export default function BaseModule({type, data, id} : NodeProps){
 
-   const [active, setActive] = useState(false)
-   const openInspector = useInspectorStore((state) => state.open)
-   const refreshInspector = useInspectorStore((state) => state.set)
-   const clearInspector = useInspectorStore((state) => state.clear)
    const module = modules[type]
-   const details = module.Detail()
+   const Detail = module.Detail
+   const { openInspector } = useInspector(<Detail key={id} />)
 
-   // Detect when content change and re-render
-   useEffect(() => {
-      if(details && active){
-         refreshInspector(details)
-      }
-   }, [details, refreshInspector, active])
-   
    const handleOnClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       event.preventDefault()
-
-      if(module.Detail){
-         openInspector(module.Detail())
-         setActive(true)
-      }
-      else{
-         clearInspector()
-      }
+      openInspector()
    }
 
    const className = `bg-white border-1 cursor-pointer hover:border-green-400 focus:border-green-400 focus:text-green-400 hover:text-green-400 
