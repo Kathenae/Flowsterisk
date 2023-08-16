69b41c68a3f1f883ac569133f30ba1554f8edf01
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 04:31:50 +0200

    Refactor Examole Detail component to use state hook and add button increment logic

--- src/modules/ExampleModule/Detail.tsx
+++ src/modules/ExampleModule/Detail.tsx
@@ -1,19 +1,28 @@
 // Component that will be used to show and edit the module details in the inspector, either when selected on the flow view or module picker
 
+import { useState } from "react";
+import Button from "../../components/Button";
 import Tabs from "../../components/Tabs";
 
 export default function Detail(){
+   const [basic, setBasic] = useState(0)
+   const [advanced, setAdvanced] = useState(0)
+   const [extra, setExtra] = useState(0)
+
    return (
-      <Tabs>
-         <Tabs.Panel name="Basic">
-            Basic
-         </Tabs.Panel>
-         <Tabs.Panel name="Advanced">
-            Advanced
-         </Tabs.Panel>
-         <Tabs.Panel name="Extra">
-            Extra
-         </Tabs.Panel>
-      </Tabs>
+   <Tabs>
+      <Tabs.Panel name='Basic'>
+         <h1>Basic Settings</h1>
+         <Button className='mt-2' primary onClick={() => setBasic(basic + 1)}>Increment {basic}</Button>
+      </Tabs.Panel>
+      <Tabs.Panel name="Advanced">
+         <h1>Advanced Settings</h1>
+         <Button className='mt-2' primary onClick={() => setAdvanced(advanced + 1)}>Increment {advanced}</Button>
+      </Tabs.Panel>
+      <Tabs.Panel name="Extra">
+         <h1>Extra Settings</h1>
+         <Button className='mt-2' primary onClick={() => setExtra(extra + 1)}>Increment {extra}</Button>
+      </Tabs.Panel>
+   </Tabs>
    )
 }
