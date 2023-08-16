e0539371745323fd59686c9d5232be68020c35ac
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 03:32:11 +0200

    Refactor InspectorStore in Flow to simplify content management
    The commit refactors InspectorStore in Flow to simplify content management by replacing setContent, setTabs, setActiveTabIndex, and clearTabs with open and set methods. The clear method is also added to clear the content. The isOpen state is now managed using toggle method.

--- src/pages/Flow/InspectorStore.tsx
+++ src/pages/Flow/InspectorStore.tsx
@@ -1,28 +1,19 @@
 import React from 'react'
 import { create } from 'zustand'
-import { TabbedPaneElement } from '../../components/Tabs'
-
-export interface InspectorTabs {
-  [key : string] : React.JSX.Element
-}
 
 interface InspectorStore {
   isOpen: boolean
+  content?: React.ReactElement,
   toggle: () => void,
-  setContent: (content: React.JSX.Element) => void,
-  content?: React.JSX.Element,
-  tabs?: TabbedPaneElement,
-  setTabs: (tabs: TabbedPaneElement) => void,
-  clearTabs: () => void,
-  activeTabIndex?: string,
-  setActiveTabIndex: (index : string) => void,
+  open: (content : React.ReactElement) => void,
+  set: (content: React.ReactElement) => void,
+  clear: () => void,
 }
 
 export const useInspectorStore = create<InspectorStore>()((set) => ({
   isOpen: false,
   toggle: () => set((state) => ({isOpen: !state.isOpen})),
-  setContent: (content: React.JSX.Element) => set(() => ({content, isOpen: true})),
-  setTabs: (tabs: TabbedPaneElement) => set(() => ({tabs, isOpen: true})),
-  clearTabs: () => set(() => ({tabs: undefined})),
-  setActiveTabIndex: (index : string) => set(() => ({activeTabIndex: index}))
+  clear: () => set(() => ({content: undefined})),
+  open: (tabs: React.ReactElement) => set(() => ({content: tabs, isOpen: true})),
+  set: (tabs: React.ReactElement) => set(() => ({content: tabs}))
 }))
