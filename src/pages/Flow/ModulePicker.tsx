7d08166434e646f1bf7468f05add5bbe0a95ba6b
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 11:56:08 +0200

    refactor ModulePicker component and introduce ModuleList and ModuleItem components
    - Refactored the ModulePicker component to use the new ModuleList and ModuleItem components
    - Introduced state for filtering modules in the ModuleList component
    - Added onClick handler to ModuleItem component to set active module
    - Implemented conditional rendering of ModuleList and active module's List component

--- src/pages/Flow/ModulePicker.tsx
+++ src/pages/Flow/ModulePicker.tsx
@@ -1,36 +1,75 @@
-import { PropsWithChildren } from 'react'
-import Button from '../../components/Button';
-import { useModulePickerState } from './modulePickerStore';
-import { Module } from '../../modules/types';
+import { PropsWithChildren, useState } from 'react'
+import Button from '../../components/Button'
+import { useModulePickerState } from './modulePickerStore'
+import { Module } from '../../modules/types'
 import modules from '../../modules';
 
-function ModuleItem({ className, module }: PropsWithChildren<{ className?: string, module: Module}>) {
+export function ModuleList({children, onFilter} : PropsWithChildren<{onFilter: (filter : string) => void}>) {
+
+  const [filter, setFilter] = useState('')
+  
+  const handleOnChange = (value : string) => {
+    setFilter(value)
+    onFilter(value.toLowerCase())
+  }
+
+  return (
+    <>
+      <div className='px-4 mt-4'>
+        <div className='relative'>
+          <input value={filter} onChange={(event) => handleOnChange(event.target.value)} placeholder='Filter Modules' className='w-full px-2 py-1 border-2 border-gray-200 rounded-md focus:outline-none' type="text" />
+          <i className='i-carbon-search text-gray-400 absolute top-2.5 right-2' />
+        </div>
+      </div>
+
+      <div className='grid grid-cols-1 gap-2 px-4 mt-4 max-h-[77%] overflow-y-auto scrollable mx-1'>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export function ModuleItem({ className, onClick, label, module }: PropsWithChildren<{ className?: string, onClick : () => void, label?: string, module: Module}>) {
   
   const onDragStart = (event : React.DragEvent<HTMLButtonElement>) => {
     event.dataTransfer.setData("module", JSON.stringify(module));
   }
   
   return (
-    <Button onDragStart={onDragStart} draggable className={`min-w-fit text-sm text-center ${className}`}>
+    <Button onClick={onClick} onDragStart={onDragStart} draggable className={`min-w-fit text-sm flex flex-row items-center justify-start h-18 ${className}`}>
       <i className={`${module.iconClass} text-2xl`} />
-      <p>
-        {module.label}
+      <p className='whitespace-normal max-w-54 ml-2 text-left mt-2 overflow-ellipsis overflow-hidden'>
+        {label ?? module.label}
       </p>
     </Button>
   )
 }
 
+
 export default function ModulePicker() {
-  const toggle = useModulePickerState((state) => state.toggle);
-  const isOpen = useModulePickerState((state) => state.isOpen);
+  const toggle = useModulePickerState((state) => state.toggle)
+  const isOpen = useModulePickerState((state) => state.isOpen)
+  const [activeModule, setActiveModule] = useState<Module | null>()
+  const [filteredModules, setFilteredModules] = useState(Object.values(modules)) 
+
+  const handleOnFilter = (filter : string) => {
+    const filtered = Object.values(modules).filter(m => m.label.toLowerCase().includes(filter))
+    setFilteredModules(filtered)
+  }
 
   return (
     <div className={`absolute top-0 h-full flex z-10 items-center pointer-events-none transition-all duration-300 ${isOpen? 'left-4' : '-left-100'}`}>
       <div className={`bg-white rounded-2xl border-gray-300 border shadow-lg h-150 w-80 pointer-events-auto`}>
 
         <div className='relative text-left pl-10'>
-          <i className='i-carbon-application text-2xl absolute top-0 left-4 !p-1 border-none' />
-          <h1 className='px-4 mt-4 text-lg font-semibold'>Modules</h1>
+          {activeModule ?
+            <Button className='min-w-fit absolute -top-1 left-4 !p-1 border-none' onClick={() => setActiveModule(null)}>
+              <i className='i-carbon-arrow-left text-2xl border-none' />
+            </Button> 
+            :
+            <i className='i-carbon-application text-2xl absolute top-0 left-4 !p-1 border-none' /> 
+          }
+          <h1 className='px-4 mt-4 text-lg font-semibold'>{activeModule?.label ?? 'Modules'}</h1>
           <Button className='min-w-fit absolute -top-1 right-4 !p-1 border-none' onClick={toggle}>
             <i className='i-carbon-close text-2xl' />
           </Button>
@@ -38,22 +77,21 @@ export default function ModulePicker() {
 
         <hr className='mt-2' />
 
-        <div className='px-4 mt-4'>
-          <div className='relative'>
-            <input placeholder='Filter Modules' className='w-full px-2 py-1 border-2 border-gray-200 rounded-md focus:outline-none' type="text" />
-            <i className='i-carbon-search text-gray-400 absolute top-2.5 right-2' />
-          </div>
-        </div>
+        {!activeModule &&
+          <>
+            <ModuleList onFilter={handleOnFilter}>
+                {filteredModules.map((module) => {
+                  return (
+                    <ModuleItem onClick={() => setActiveModule(module)} key={module.type} module={module} />
+                  )
+                })}
+            </ModuleList>
+          </>
+        }
 
-        <div className='grid grid-cols-2 space px-4 mt-4 max-h-[77%] overflow-y-auto scrollable mx-1'>
-          {Object.keys(modules).map((moduleType) => {
-            const module = modules[moduleType]
-            return (
-              <ModuleItem key={moduleType} module={module} className='mb-4 mx-1 h-24' />
-            )
-          })}
-        </div>
-
+          {activeModule &&
+            <activeModule.List />
+          }
       </div>
     </div>
   )
