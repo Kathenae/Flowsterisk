aee998dd7e994da97598da66e3c3aa821790814f
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 19:54:55 +0200

    update  BaseModule  styling and handle colors in BaseModule component
    The commit fixes the styling of the module component by updating the class names to improve the hover and focus effects.

--- src/components/Modules/BaseModule.tsx
+++ src/components/Modules/BaseModule.tsx
@@ -12,21 +12,21 @@ export default function BaseModule({ data, id} : NodeProps){
       openInspector()
    }
 
-   const className = `bg-white border-1 cursor-pointer hover:border-green-400 focus:border-green-400 focus:text-green-400 hover:text-green-400 
-   rounded border-black w-38 h-42 text-center overflow-hidden flex flex-col items-center group`
+   const className = `bg-white border-1 cursor-pointer hover:border-green-700 focus:border-green-500 focus:text-green-500 hover:text-green-700 
+   rounded border-black shadow-lg w-38 h-42 text-center overflow-hidden flex flex-col items-center group`
 
    return (
       <button 
          className={className}
          onClick={handleOnClick}
       >
-         <div className='w-full p-2 flex items-center justify-center border-b border-black group-hover:border-green-400'>
+         <div className='w-full p-2 flex items-center justify-center border-b border-black group-focus:border-green-500 group-hover:border-green-700'>
             <h1 className='whitespace-nowrap font-medium'>{module.label}</h1>
          </div>
          <div className='mt-4'>
             <p>{module.instance?.label ?? "New " + module.label}</p>
          </div>
-         <i className={`${module.iconClass} mr-2 text-3xl text-gray-200 absolute bottom-3 group-hover:text-green-200`}/>
+         <i className={`${module.iconClass} mr-2 text-3xl text-gray-200 absolute bottom-3 group-focus:text-green-200 group-hover:text-green-100`}/>
          <Handle id='1' type='source' position={Position.Right} />
          <Handle id='2' type='target' position={Position.Left} />
       </button>
