ea8e0c7fceb712ed30583569882ab07322c1dbd4
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 19:33:28 +0200

    update  `modules/index.ts`

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,12 +1,12 @@
 import { Module } from "./types"
 
 const modules = {}  as {
-   [key : string] : Module
+   [key : string] : Module<unknown>
 }
 
 const imports = import.meta.glob('./*/index.tsx', {eager: true})
 for (const path in imports){
-   const { default : module } = (await imports[path]) as { default : Module }
+   const { default : module } = (await imports[path]) as { default : Module<unknown> }
 
    // Make the name of the module directory be the name of its type
    const moduleName = path.split("/")[1]
