3bed235778e694c0f1c95af43ef349f09f709c9d
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 11:55:33 +0200

    Refactor list function to return a new set of items.

--- src/modules/ExampleModule/API.ts
+++ src/modules/ExampleModule/API.ts
@@ -6,9 +6,15 @@ async function list()
 
    return [
       {id: 1, label: "Item 1"},
-      {id: 1, label: "Item 1"},
-      {id: 1, label: "Item 1"},
-      {id: 1, label: "Item 1"},
+      {id: 2, label: "Item 2"},
+      {id: 3, label: "Item 3"},
+      {id: 4, label: "Item 4"},
+      {id: 5, label: "Item 5"},
+      {id: 6, label: "Item 6"},
+      {id: 7, label: "Item 7"},
+      {id: 8, label: "Item 8"},
+      {id: 9, label: "Item 9"},
+      {id: 10, label: "Item 10"},
    ] as unknown[] 
 }
 
