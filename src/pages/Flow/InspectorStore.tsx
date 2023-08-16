39e20f905d56ff5e1e9d07cefb8edf864766f38e
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-16 04:30:47 +0200

    Add useInspector hook for managing inspector instances

--- src/pages/Flow/InspectorStore.tsx
+++ src/pages/Flow/InspectorStore.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { create } from 'zustand'
 
 interface InspectorStore {
@@ -17,3 +17,29 @@ export const useInspectorStore = create<InspectorStore>()((set) => ({
   open: (tabs: React.ReactElement) => set(() => ({content: tabs, isOpen: true})),
   set: (tabs: React.ReactElement) => set(() => ({content: tabs}))
 }))
+
+export const useInspector = (content : React.ReactElement) => {
+  const [active, setActive] = useState(false) // Whether this specific inspector instance is active
+  const toggle = useInspectorStore((state) => state.toggle)
+  const open = useInspectorStore((state) => state.open)
+  const refreshInspector = useInspectorStore((state) => state.set)
+  
+  useEffect(() => {
+     if(content && active) {
+        refreshInspector(content)
+     }
+  }, [content, active, refreshInspector])
+
+  function openInspector(){
+    if(content){
+      open(content)
+      setActive(true)
+    }
+  }
+
+  function toggleInspector(){
+    toggle()
+  }
+
+  return {openInspector, toggleInspector}
+}
