b64c44eb522069d98eda5d58c53d3d90ed0a2eb5
Ronald <RonaldWilsonMachado@gmail.com>
2023-08-18 20:52:46 +0200

    feat(BaseNode): Toggle inspector when node is deselected
    This commit adds functionality to toggle the inspector when the node is deselected. The `useEffect` hook is used to check the `selected` prop and call the `toggleInspector` function accordingly. This ensures that the inspector is closed when the node is deselected.
    
    The class name for the `BaseNode` component is also updated to conditionally apply a border and text color based on the `selected` prop. This provides visual feedback to indicate the selected state of the node.

--- src/modules/BaseNode.tsx
+++ src/modules/BaseNode.tsx
@@ -1,32 +1,39 @@
 import { Handle, NodeProps, Position } from 'reactflow'
 import { useInspector } from '../pages/Flow/InspectorStore'
 import { Module, ModuleInstance } from './types'
+import {useEffect} from 'react'
 
-export default function BaseNode({ data, id} : NodeProps){
+export default function BaseNode({ data, id, selected} : NodeProps){
 
    const module  = data as Module<ModuleInstance>
-   const { openInspector } = useInspector(<module.Detail key={id} module={module}/>)
+   const { openInspector, toggleInspector } = useInspector(<module.Detail key={id} module={module}/>)
 
    const handleOnClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       event.preventDefault()
       openInspector()
    }
 
-   const className = `bg-white dark:bg-dark-400 dark:text-gray-100 border-1 dark:border-dark-100 cursor-pointer hover:border-brand-700 focus:border-brand-500 focus:text-brand-500 hover:text-brand-700 
-   rounded border-black shadow-lg w-38 h-42 text-center overflow-hidden flex flex-col items-center group`
+   useEffect(() => {
+      if(!selected){
+         toggleInspector()
+      }
+   }, [selected, toggleInspector])
+
+   const className = `bg-white dark:bg-dark-400 dark:text-gray-100 border-1 dark:border-dark-100 cursor-pointer hover:border-brand-700 hover:text-brand-700 
+   rounded border-black shadow-lg w-38 h-42 text-center overflow-hidden flex flex-col items-center group ${selected && '!border-brand-500 !text-brand-500'}`
 
    return (
       <button 
          className={className}
          onClick={handleOnClick}
       >
-         <div className='w-full p-2 flex items-center justify-center border-b border-black dark:border-dark-100 group-focus:border-brand-500 group-hover:border-brand-700'>
+         <div className={`w-full p-2 flex items-center justify-center border-b border-black dark:border-dark-100 ${selected && '!border-brand-500'}  group-hover:border-brand-700`}>
             <h1 className='whitespace-nowrap font-medium'>{module.label}</h1>
          </div>
          <div className='mt-4'>
             <p>{module.instance?.label ?? "New " + module.label}</p>
          </div>
-         <i className={`${module.iconClass} mr-2 text-3xl text-gray-200 absolute bottom-3 group-focus:text-brand-200 group-hover:text-brand-100`}/>
+         <i className={`${module.iconClass} mr-2 text-3xl text-gray-200 absolute bottom-3 ${selected && '!text-brand-200'}  group-hover:text-brand-100`}/>
          <Handle id='1' type='source' position={Position.Right} />
          <Handle id='2' type='target' position={Position.Left} />
       </button>
