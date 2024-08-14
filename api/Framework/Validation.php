9840f1464b485a4d13011bef7f166029fc3a6638
Ronald <Ronald43.github@gmail.com>
2024-08-14 01:46:28 +0000

    created Validation class and messages

---
+++ api/Framework/Validation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Framework;
+
+use Illuminate\Validation\Factory;
+use Illuminate\Filesystem;
+use Illuminate\Translation;
+
+class Validation {
+    public static function make($data, $rules, $messages = [], $attributes = []) {
+        $default_messages = require('./Messages/en/validation.php');
+        $filesystem = new Filesystem\Filesystem();
+        $fileLoader = new Translation\FileLoader($filesystem, '');
+        $translator = new Translation\Translator($fileLoader, 'en_US');
+        $factory = new Factory($translator);
+        return $factory->make($data, $rules, array_merge($default_messages, $messages), $attributes);
+    }
+}
